424c18271229c3f3645ff1a1a77991873b33a653
ailingangel <angel20081018@163.com>
2018-05-16 00:09:53 +0800

    简单配置

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+var config = {
+    entry: {
+        main: './main' //从这个文件开始寻找依赖
+    },
+    output: {
+        path: path.join(__dirname, './dist'), //存放打包文件输出目录
+        publicPath: '/dist', //制定资源文件引用目录
+        filename: 'main.js' //生成的文件名
+    }
+};
+
+module.exports = config;
+//之前写成了export 就报错了说config file found but no entry
+//之前还使用了webpack4 总是报错误解析./src文件失败，开始很纳闷没写过./src路径查了之后发现是webpack4默认要找src文件下的index.js作为入口文件
+//然后果断降级为webpack2
