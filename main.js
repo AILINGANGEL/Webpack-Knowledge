a3d8c8d7a097f56eeae87c818c8ba481ce04d53c
ailingangel <angel20081018@163.com>
2018-05-22 23:06:27 +0800

    vuex

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 import Vue from 'vue';
 import App from './app.vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import './style.css';
 Vue.use(VueRouter);
+Vue.use(Vuex);
 const routes = [{
     path: '/index',
     meta: {
@@ -43,8 +45,91 @@ router.afterEach((to, from, next) => {
     window.scrollTo(0,0); //回到页面顶端
 });
 
+const moduleA = {
+    state: {},
+    mutations: {
+        a(state){
+            //state是moduleA的state
+        }
+    },
+    actions: {},
+    getters: {
+        a(state, getters, rootState) {
+            //rootState就是根的state
+        }
+    }
+}
+
+const moduleB = {
+    state: {},
+    mutations: {},
+    actions: {},
+    getters: {}
+}
+
+const store = new Vuex.Store({
+    // state中的数据只能读取不能修改
+    state: {
+        count: 0,
+        list: [1, 5, 8 , 10, 20]
+    },
+    mutations: {
+        // 这里面不要去异步操作数据
+        increment(state, n=1) {
+            state.count += n;
+        },
+        decrease(state, n=1) {
+            state.count -=n;
+        }
+    },
+    getters: {
+        // getters可以依赖其他的getter
+        filterList: (state, getters) => {
+            return state.list.filter(item=>item<10);
+        }
+    },
+    actions: {
+        // 做异步操作,不直接修改state,通过mutations来修改数据
+        increment(context, callback) {
+            //pending/resolved/rejected
+            // const p = new Promise((resolve, reject)=>{
+            //     const random = Math.random();
+            //     if (random > 0.5) {
+            //         resolve(random);
+            //     } else {
+            //         reject(random);
+            //     }
+            // });
+
+            // p.then(val=>{
+            //     console.log(val);
+            // }).catch(val => {
+            //     console.log(val);
+            // })
+
+
+            //异步代码xxxx
+            // context.commit('increment');
+            return new Promise((resolve, reject)=>{
+                setTimeout(()=>{
+                    context.commit('increment');
+                    resolve();
+                }, 1000)
+            })
+            // setTimeout(()=>{
+            //     callback();
+            // }, 1000);
+        }
+    },
+    modules: {
+        a: moduleA,
+        b: moduleB
+    }
+});
+
 new Vue({
     el: '#app',
     router: router,
+    store: store,
     render: h => h(App)
 })
--- views/index.vue
+++ views/index.vue
@@ -1,12 +1,43 @@
 <template>
     <div>
     <div>首页</div>
+    {{count}}
+    {{list}}
+    <button @click="handleClick">+2</button>
+    <button @click="handleD">+1</button>
+    <br/>
+    <button @click="handleActionAdd">action +1</button>
     <!-- replace属性开启不记录历史记录的功能，浏览器的返回按钮不能回到上一个页面 -->
-    <router-link to="/about" tag="li" replace>跳转到about</router-link>
+    <!-- <router-link to="/about" tag="li" replace>跳转到about</router-link> -->
 </div>
 </template>
 <script>
     export default {
+        computed: {
+            count() {
+                // return this.$store.state.a.count;
+                return this.$store.state.count;
+            },
+            // list() {
+            //     return this.$store.state.list.filter(item=> item<10);
+            // }
+            list() {
+                return this.$store.getters.filterList;
+            }
+        },
+        methods: {
+            handleClick() {
+                this.$store.commit('increment', 2);
+            },
+            handleD() {
+                this.$store.commit('decrease');
+            },
+            handleActionAdd() {
+                this.$store.dispatch('increment').then((data)=>{
+                    console.log(data);
+                });
+            }
+        }
 
     }
 </script>
